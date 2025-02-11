d0d9c0a278b214f82db35622e3a4a880ec61a947
RussellZhou-hub <938311288@qq.com>
2025-02-11 18:31:40 +0800

    engine factory

---
+++ NovaEngine/Engine/Core/Engine.h
@@ -0,0 +1,15 @@
+#pragma once
+
+class FEngine
+{
+public:
+	virtual int PreInit();
+	virtual int Init();
+	virtual int PostInit();
+
+	virtual void Tick();
+
+	virtual int PreExit();
+	virtual int Exit();
+	virtual int PostExit();
+};
---
+++ NovaEngine/Engine/EngineFactory.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "Core/Engine.h"
+
+class FEngineFactory
+{
+public:
+	static FEngine* CreateEngine();
+};
